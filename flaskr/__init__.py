c43d7f40b4a18ed332f6fb5706dab91b7d640427
Romain Girard <romainr.girardg@gmail.com>
2020-06-19 08:53:34 +0200

    Create the blog blueprint

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -34,5 +34,9 @@ def create_app(test_config=None):
     # Register the authentication blueprint
     app.register_blueprint(auth.bp)
 
+    # Register the blog blueprint
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+
     return app
     
