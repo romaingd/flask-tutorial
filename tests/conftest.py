935f8b15a02cc08f0c35362dcf2fdfa4e1966d74
Romain Girard <romainr.girardg@gmail.com>
2020-06-20 17:09:59 +0200

    Create factory and database tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@ import tempfile
 
 import pytest
 from flaskr import create_app
-from flaskr.auth import get_db, init_db
+from flaskr.db import get_db, init_db
 
 with open(os.path.join(os.path.dirname(__file__), 'data.sql'), 'rb') as f:
     _data_sql = f.read().decode('utf8')
