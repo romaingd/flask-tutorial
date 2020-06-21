094d594a515ae23a8d874492775ba7dc56312fd4
Romain Girard <romainr.girardg@gmail.com>
2020-06-21 03:12:18 +0200

    Create authentication tests

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -10,7 +10,7 @@ from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
-@bp.route('/register', methods=['GET', 'POST'])
+@bp.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
         username = request.form['username']
@@ -19,14 +19,14 @@ def register():
         error = None
 
         if not username:
-            error = 'Username is required'
+            error = 'Username is required.'
         elif not password:
-            error = 'Password is required'
+            error = 'Password is required.'
         elif db.execute(
             'SELECT id FROM user WHERE username = ?', (username,)
         ).fetchone() is not None:
-            error = 'User {} is already registered'.format(username)
-        
+            error = 'User {} is already registered.'.format(username)
+
         if error is None:
             db.execute(
                 'INSERT INTO user (username, password) VALUES (?, ?)',
@@ -39,30 +39,29 @@ def register():
 
     return render_template('auth/register.html')
 
-@bp.route('/login', methods=['GET', 'POST'])
+@bp.route('/login', methods=('GET', 'POST'))
 def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         db = get_db()
         error = None
-
         user = db.execute(
             'SELECT * FROM user WHERE username = ?', (username,)
         ).fetchone()
 
         if user is None:
-            error = 'Incorrect username'
+            error = 'Incorrect username.'
         elif not check_password_hash(user['password'], password):
-            error = 'Incorrect password'
-        
+            error = 'Incorrect password.'
+
         if error is None:
             session.clear()
             session['user_id'] = user['id']
             return redirect(url_for('index'))
-        
+
         flash(error)
-    
+
     return render_template('auth/login.html')
 
 @bp.before_app_request
@@ -85,7 +84,7 @@ def login_required(view):
     @functools.wraps(view)
     def wrapped_view(**kwargs):
         if g.user is None:
-            return redirect(url_for('login'))
+            return redirect(url_for('auth.login'))
 
         return view(**kwargs)
     
